cf4b82577b1ce17635f4a2b2ce458b06c2e9d8a8
eme40 <anayoeric40@gmail.com>
2026-01-25 15:26:15 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // eslint-disable-next-line no-undef
+  base: process.env.VITE_BASE_PATH || "/Tender-heart",
   server: {
     port: 3000
   }
