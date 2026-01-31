5c1bbb43194cdd82a80361bce50391829e8a19b6
eme40 <anayoeric40@gmail.com>
2026-01-31 13:44:18 +0100

    app jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -14,23 +14,22 @@ import PhotoCategory from "./pages/PhotoCategory";
 import PhotoGallery from "./pages/PhotoGallery";
 import ServicesPage from "./pages/ServicesPage";
 import ErrorPage from "./pages/ErrorPage"; // Import it
-
 const App = () => {
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<Layout />} errorElement={<ErrorPage />}>
-      <Route index element={<LandingPage />} />
-      <Route path="home" element={<LandingPage />} />
-      <Route path="about" element={<AboutUsPage />} />
-      <Route path="contact" element={<ContactPage />} />
-      <Route path="media" element={<PhotoPage />} />
-      <Route path="media/category" element={<PhotoCategory />} />
-      <Route path="media/gallery" element={<PhotoGallery />} />
-      <Route path="services" element={<ServicesPage />} />
-      <Route path="*" element={<ErrorPage />} /> {/* Best approach */}
-    </Route>
-  )
-);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+        <Route index element={<LandingPage />} />
+        <Route path="home" element={<LandingPage />} />
+        <Route path="about" element={<AboutUsPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="media" element={<PhotoPage />} />
+        <Route path="media/category" element={<PhotoCategory />} />
+        <Route path="media/gallery" element={<PhotoGallery />} />
+        <Route path="services" element={<ServicesPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    )
+  );
 
   return <RouterProvider router={router} />;
 };
