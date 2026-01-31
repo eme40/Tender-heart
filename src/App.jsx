65063b0b95b9943874c6bc2cef988c963ec7d5ba
eme40 <anayoeric40@gmail.com>
2026-01-31 13:56:24 +0100

    error page final update

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,24 @@ import PhotoPage from "./pages/PhotoPage";
 import PhotoCategory from "./pages/PhotoCategory";
 import PhotoGallery from "./pages/PhotoGallery";
 import ServicesPage from "./pages/ServicesPage";
-import ErrorPage from "./pages/ErrorPage"; // Import it
+import ErrorPage from "./pages/ErrorPage";
+
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
-        <Route index element={<LandingPage />} />
-        <Route path="home" element={<LandingPage />} />
-        <Route path="about" element={<AboutUsPage />} />
-        <Route path="contact" element={<ContactPage />} />
-        <Route path="media" element={<PhotoPage />} />
-        <Route path="media/category" element={<PhotoCategory />} />
-        <Route path="media/gallery" element={<PhotoGallery />} />
-        <Route path="services" element={<ServicesPage />} />
+      <>
+        <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+          <Route index element={<LandingPage />} />
+          <Route path="home" element={<LandingPage />} />
+          <Route path="about" element={<AboutUsPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="media" element={<PhotoPage />} />
+          <Route path="media/category" element={<PhotoCategory />} />
+          <Route path="media/gallery" element={<PhotoGallery />} />
+          <Route path="services" element={<ServicesPage />} />
+        </Route>
         <Route path="*" element={<ErrorPage />} />
-      </Route>
+      </>
     )
   );
 
